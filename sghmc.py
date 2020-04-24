187ecc020231393d5efbdbe37bd01511b172393c
JiajunSong629 <jiajun_song0928@outlook.com>
2020-04-24 04:16:45 -0400

    add comments

--- sghmc.py
+++ sghmc.py
@@ -1,7 +1,6 @@
 import numpy as np
 from preprocess import create_batch
 
-
 def sghmc(gradU, dataset, batch_size, start_states, alpha=None, eta=None, n_epochs=500, n_burn=50, random_seed=42):
     '''
     SGHMC cleaner version, core implementation
@@ -49,18 +48,27 @@ def sghmc(gradU, dataset, batch_size, start_states, alpha=None, eta=None, n_epoc
     proposal_samples: 2-d array, shape = (n_epochs - n_burn, n_params)
         the parameter vector samples obtained by SGHMC following the target posterior distribution
     '''
-    
+
+    # check some options
+    assert batch_size >= 1, 'batch size has to be >= 1'
+    assert n_burn >= 0, 'n_burn has to be >= 0'
+    assert alpha >= 0, 'momentum decay has to be >= 0'
+    assert alpha <= 1, 'momentum decay has to be <= 1'
+
+    ## random state
     RSTATE = np.random.RandomState(int(random_seed))
     
+    ## parameter vector dimension and number of samples
     n_samples = dataset.shape[0]
     n_params = start_states.shape[0]
+    assert batch_size <= n_samples, 'batch_size cannot be bigger than number of samples'
     
     ## placeholder for proposal_samples
     proposal_samples = np.zeros((n_epochs, n_params))
     proposal_samples[0] = start_states
     
     ## hyperparameter
-    ## details about tunning please refer to report chapter 4
+    ## please refer to report chapter 4 for details about tunning
     alpha = 0.1 if alpha is None else alpha
     eta = 2 * 0.01 / n_samples if eta is None else eta
     bhat = 0
@@ -69,17 +77,21 @@ def sghmc(gradU, dataset, batch_size, start_states, alpha=None, eta=None, n_epoc
     ## data
     minibatch_data, n_batches = create_batch(dataset, batch_size)
     
-
+    ## main loop
     for i in range(n_epochs-1):
         cur_states = proposal_samples[i]
+        
+        ## resample momentum at each new epoch
         momentum = np.sqrt(eta) * RSTATE.randn(n_params)
         
+        ## sghmc sampler
         for j in range(n_batches):
             cur_states = cur_states + momentum
             gradU_batch = gradU(cur_states, minibatch_data[:,:,j], n_samples/batch_size)
             momentum = (1-alpha) * momentum - eta * gradU_batch.flatten() \
                                             + np.dot(Sigma, RSTATE.randn(n_params))
         
+        ## store results
         proposal_samples[i+1] = cur_states
     
     return proposal_samples[n_burn: , :]
